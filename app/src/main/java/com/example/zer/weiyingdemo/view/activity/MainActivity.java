b5e64560aca46a91033e701a6802f7895b6a7d52
gracezer <2939406732@qq.com>
2018-07-06 12:16:33 +0800

    first tijiao

--- app/src/main/java/com/example/zer/weiyingdemo/view/activity/MainActivity.java
+++ app/src/main/java/com/example/zer/weiyingdemo/view/activity/MainActivity.java
@@ -1,19 +1,18 @@
 package com.example.zer.weiyingdemo.view.activity;
-
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.zer.weiyingdemo.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        MainActivity mainActivity = new MainActivity();
-        
+        Log.e("aaaa", "onCreate: &&&"+"zhengenru");
+        Log.e("aaaa", "onCreate: &&&"+"wangtianqi");
+        Log.e("aaaa", "onCreate: &&&"+"lihaofan");
     }
+    
 }
