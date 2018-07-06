63369fe14ee6c6cd9423565eeb545f893ca2773e
gracezer <2939406732@qq.com>
2018-07-06 19:11:51 +0800

    first 头部字体

--- app/src/main/java/com/example/zer/weiyingdemo/view/activity/MainActivity.java
+++ app/src/main/java/com/example/zer/weiyingdemo/view/activity/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.zer.weiyingdemo.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import com.example.zer.weiyingdemo.R;
 import com.example.zer.weiyingdemo.view.fragment.fourFragment;
 import com.example.zer.weiyingdemo.view.fragment.oneFragment;
@@ -12,12 +15,14 @@ import com.hjm.bottomtabbar.BottomTabBar;
 public class MainActivity extends AppCompatActivity {
 
     private BottomTabBar bottom_tabbar;
+    private TextView include_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottom_tabbar = findViewById(R.id.bottom_tabbar);
+        include_title = findViewById(R.id.include_title);
         bottom_tabbar.init(getSupportFragmentManager())
                 .setImgSize(50, 50)//设置ICON图片的尺寸
                 .setFontSize(15)//设置文字的尺寸
@@ -27,6 +32,22 @@ public class MainActivity extends AppCompatActivity {
                 .addTabItem("发现", R.drawable.fancy_select, R.drawable.fancy, threeFragment.class)
                 .addTabItem("我的", R.drawable.my_select, R.drawable.my, fourFragment.class)
                 .setTabBarBackgroundResource(R.drawable.bottom_bg)
+                .setOnTabChangeListener(new BottomTabBar.OnTabChangeListener() {
+                    @Override
+                    public void onTabChange(int position, String name, View view) {
+                        switch (position){
+                            case 1:
+                                include_title.setText("专题");
+                                break;
+                            case 2:
+                                include_title.setText("发现");
+                                break;
+                            case 3:
+                                include_title.setText("我的");
+                                break;
+                        }
+                    }
+                })
                 .setCurrentTab(0);
     }
     
