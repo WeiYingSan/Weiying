a0b195b2c2251f08d18e0f85a60fa16673006cf3
gracezer <2939406732@qq.com>
2018-07-06 14:27:53 +0800

    first xiugai1

--- app/src/main/java/com/example/zer/weiyingdemo/view/activity/MainActivity.java
+++ app/src/main/java/com/example/zer/weiyingdemo/view/activity/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.zer.weiyingdemo.view.activity;
-import android.os.Handler;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 import com.example.zer.weiyingdemo.R;
-
 import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,9 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         GregorianCalendar gregorianCalendar = new GregorianCalendar();
-
+        Log.e("aaa", "onCreate: "+"zer");
+        Log.e("aaa", "onCreate: "+"zer1");
+        Log.e("aaa", "onCreate: "+"zer2");
+        Log.e("aaa", "onCreate: "+"zer3");
     }
     
 }
