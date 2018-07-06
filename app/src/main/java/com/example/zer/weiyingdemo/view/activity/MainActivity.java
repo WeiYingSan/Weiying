24a56d3de416e2c6fc9167db894ee1d742756ee7
gracezer <2939406732@qq.com>
2018-07-06 15:29:51 +0800

    first 忽略

--- app/src/main/java/com/example/zer/weiyingdemo/view/activity/MainActivity.java
+++ app/src/main/java/com/example/zer/weiyingdemo/view/activity/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.zer.weiyingdemo.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import com.example.zer.weiyingdemo.R;
-import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
