b81dd9782540975bf4fa96ae9a1cad4af4a502fb
s <573900604@qq.com>
2018-07-06 19:09:43 +0800

    侧拉

--- app/src/main/java/com/example/zer/weiyingdemo/view/activity/MainActivity.java
+++ app/src/main/java/com/example/zer/weiyingdemo/view/activity/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.zer.weiyingdemo.view.activity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.example.zer.weiyingdemo.R;
 import com.example.zer.weiyingdemo.view.fragment.fourFragment;
 import com.example.zer.weiyingdemo.view.fragment.oneFragment;
@@ -15,12 +21,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottom_tabbar = findViewById(R.id.bottom_tabbar);
         bottom_tabbar.init(getSupportFragmentManager())
                 .setImgSize(50, 50)//设置ICON图片的尺寸
-                .setFontSize(8)//设置文字的尺寸
+                .setFontSize(15)//设置文字的尺寸
                 .setTabPadding(10,6,10)
                 .addTabItem("精选", R.drawable.found_select, R.drawable.found, oneFragment.class)
                 .addTabItem("专题", R.drawable.special_select, R.drawable.special, twoFragment.class)
