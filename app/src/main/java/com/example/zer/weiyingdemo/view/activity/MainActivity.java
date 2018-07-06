5c50c99b16c6a53571e4c780fda1d22d3648af53
gracezer <2939406732@qq.com>
2018-07-06 19:37:34 +0800

    头部字体 底部导航栏

--- app/src/main/java/com/example/zer/weiyingdemo/view/activity/MainActivity.java
+++ app/src/main/java/com/example/zer/weiyingdemo/view/activity/MainActivity.java
@@ -24,26 +24,29 @@ public class MainActivity extends AppCompatActivity {
         bottom_tabbar = findViewById(R.id.bottom_tabbar);
         include_title = findViewById(R.id.include_title);
         bottom_tabbar.init(getSupportFragmentManager())
-                .setImgSize(50, 50)//设置ICON图片的尺寸
-                .setFontSize(15)//设置文字的尺寸
-                .setTabPadding(10,6,10)
+                .setImgSize(30, 30)//设置ICON图片的尺寸
+                .setFontSize(13)//设置文字的尺寸
+                .setTabPadding(10,6,0)
                 .addTabItem("精选", R.drawable.found_select, R.drawable.found, oneFragment.class)
                 .addTabItem("专题", R.drawable.special_select, R.drawable.special, twoFragment.class)
                 .addTabItem("发现", R.drawable.fancy_select, R.drawable.fancy, threeFragment.class)
                 .addTabItem("我的", R.drawable.my_select, R.drawable.my, fourFragment.class)
-                .setTabBarBackgroundResource(R.drawable.bottom_bg)
+                .setTabBarBackgroundResource(R.drawable.aaa)
                 .setOnTabChangeListener(new BottomTabBar.OnTabChangeListener() {
                     @Override
                     public void onTabChange(int position, String name, View view) {
                         switch (position){
+                            case 0:
+                                include_title.setText(name);
+                                break;
                             case 1:
-                                include_title.setText("专题");
+                                include_title.setText(name);
                                 break;
                             case 2:
-                                include_title.setText("发现");
+                                include_title.setText(name);
                                 break;
                             case 3:
-                                include_title.setText("我的");
+                                include_title.setText(name);
                                 break;
                         }
                     }
