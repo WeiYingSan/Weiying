a035de3b626076b8075176953942da47e329e958
lihaofan66666 <1471851958@qq.com>
2018-07-06 22:01:04 +0800

    lhf0706four

--- app/src/main/java/com/example/zer/weiyingdemo/view/fragment/fourFragment.java
+++ app/src/main/java/com/example/zer/weiyingdemo/view/fragment/fourFragment.java
@@ -3,6 +3,7 @@ package com.example.zer.weiyingdemo.view.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +11,12 @@ import android.view.ViewGroup;
 import com.example.zer.weiyingdemo.R;
 
 public class fourFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View inflate = inflater.inflate(R.layout.fourfragment, null);
+
         return inflate;
     }
 }
