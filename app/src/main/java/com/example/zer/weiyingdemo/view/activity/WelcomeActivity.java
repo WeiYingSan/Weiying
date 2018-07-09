f1ca46d6879023841a5acbc085c3e0ac806533e6
lihaofan66666 <1471851958@qq.com>
2018-07-09 09:12:45 +0800

    ll

--- app/src/main/java/com/example/zer/weiyingdemo/view/activity/WelcomeActivity.java
+++ app/src/main/java/com/example/zer/weiyingdemo/view/activity/WelcomeActivity.java
@@ -20,7 +20,7 @@ public class WelcomeActivity extends AppCompatActivity{
         setContentView(R.layout.activity_welcome);
 
         iv_welcome_bg = findViewById(R.id.iv_welcome_bg);
-        iv_welcome_bg.setImageResource(R.drawable.a);
+        iv_welcome_bg.setImageResource(R.drawable.welcome);
         iv_welcome_bg.animate().scaleX(1.12f).scaleY(1.12f).setDuration(2000).setStartDelay(100).start();
 
         Timer timer = new Timer();
