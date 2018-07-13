6b4d9d9a821987a71a80e2cd641828616513ff78
s <573900604@qq.com>
2018-07-13 20:51:36 +0800

    周五晚三节课

--- app/src/main/java/com/example/zer/weiyingdemo/view/activity/SousuoToActivity.java
+++ app/src/main/java/com/example/zer/weiyingdemo/view/activity/SousuoToActivity.java
@@ -3,8 +3,16 @@ package com.example.zer.weiyingdemo.view.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.zer.weiyingdemo.R;
+import com.example.zer.weiyingdemo.model.bean.FuzzyQueryBean;
+import com.example.zer.weiyingdemo.model.http.RetrofitUtils;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class SousuoToActivity extends AppCompatActivity {
 
@@ -18,6 +26,35 @@ public class SousuoToActivity extends AppCompatActivity {
         Intent intent1 = new Intent();
         intent1.putExtra("lishiback",editdata);
         setResult(12,intent1);
+
+        //网络请求
+        queryinternet(editdata);
+    }
+
+    private void queryinternet(String key) {
+        RetrofitUtils.getInstance().getApi()
+                .getVideoByKeyWord(key)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<FuzzyQueryBean>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+            }
+            @Override
+            public void onNext(FuzzyQueryBean value) {
+                if(value.getCode().equals(200)){
+                   // value.getRet().
+                }else{
+                    Toast.makeText(SousuoToActivity.this, "请求数据失败!", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onError(Throwable e) {
+            }
+            @Override
+            public void onComplete() {
+            }
+        });
     }
 
     @Override
